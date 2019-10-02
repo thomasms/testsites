9d7c88b42d20dd2ab77a8c5dd910cc0088fd4c51
thomasms <stainer.tom@gmail.com>
2019-10-02 21:41:38 +0100

    Added button to test site

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { instanceOf } from 'prop-types';
 import { withCookies, Cookies } from 'react-cookie';
 import './App.css';
 
-const COUNT_LIMIT = 200;
+const COUNT_LIMIT = 100;
 
 // can we use React hooks instead??
 // probably but for now we do it the old 
@@ -32,7 +32,10 @@ class App extends React.Component {
 
    render(){
        var hidden = this.state.count > COUNT_LIMIT ? 
-            <div>You are in!</div> : 
+            <div>
+              <div>You are in!</div> 
+              <button onClick={() => {window.location.href="https://www.glastonburyfestivals.co.uk/";}}>Click me</button>
+              </div>: 
             <p>
               Number of hits: {this.state.count}
             </p>;
